cce599ed9775f29a7774826959b5c973007d4399
mooncat-greenpy <mooncat.greenpy@gmail.com>
2022-11-12 11:27:45 +0900

    Fix file func mapping

--- src/main/java/golanganalyzerextension/FileDetailProvider.java
+++ src/main/java/golanganalyzerextension/FileDetailProvider.java
@@ -36,6 +36,27 @@ public class FileDetailProvider extends ComponentProviderAdapter {
 			return panel;
 		}
 
+		private void add_line_func(String file_name, GolangFunction func, Map<Long, String> line_func_map) {
+			Map<Integer, FileLine> file_line_map=func.get_file_line_comment_map();
+			if(!file_line_map.containsKey(0) || !file_name.equals(file_line_map.get(0).get_file_name())) {
+				return;
+			}
+
+			long first_line=file_line_map.get(0).get_line_num();
+			long last_line=first_line;
+			FileLine[] file_line_arr=file_line_map.values().toArray(new FileLine[0]);
+			Arrays.sort(file_line_arr, (a, b) -> (int)a.get_line_num() - (int)b.get_line_num());
+			for(FileLine file_line : file_line_arr) {
+				if(!file_line.get_file_name().equals(file_name)) {
+					continue;
+				}
+				if(file_line.get_line_num()<first_line || file_line.get_line_num()>last_line+10) {
+					continue;
+				}
+				last_line=file_line.get_line_num()>last_line?file_line.get_line_num():last_line;
+				line_func_map.put(file_line.get_line_num(), func.get_func_name());
+			}
+		}
 		private JScrollPane create_func_table(String file_name) {
 			if(gae_tool==null) {
 				return new JScrollPane();
@@ -44,12 +65,9 @@ public class FileDetailProvider extends ComponentProviderAdapter {
 			String[] columns={"Line", "FuncName"};
 			Map<Long, String> line_func_map = new HashMap<>();
 			for(GolangFunction func : gae_tool.get_function_list()) {
-				for(FileLine file_line : func.get_file_line_comment_map().values()) {
-					if(file_line.get_file_name().equals(file_name)) {
-						line_func_map.put(file_line.get_line_num(), func.get_func_name());
-					}
-				}
+				add_line_func(file_name, func, line_func_map);
 			}
+
 			Object[] map_key=line_func_map.keySet().toArray();
 	        Arrays.sort(map_key);
 			Object[][] data=new Object[line_func_map.size()][2];
